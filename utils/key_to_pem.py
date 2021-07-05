0d141d7bc342550ba834261eb32d4f8ca6b46d52
sandesh sukubhattu <sukubhattusandesh8@gmail.com>
2021-07-05 09:30:51 +0545

    Add password in private key

--- utils/key_to_pem.py
+++ utils/key_to_pem.py
@@ -1,8 +1,18 @@
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
+from .encode_to_utf8 import encode_str_utf8
 
-private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048, backend=default_backend())
+import os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+
+PASSWORD = os.environ.get("PASSWORD")
+
+private_key = rsa.generate_private_key(
+    public_exponent=65537, key_size=2048, backend=default_backend()
+)  # default backend is used automatically. RSABackend is optional backend
 public_key = private_key.public_key()
 
 
@@ -10,7 +20,7 @@ def write_private_key():
     return private_key.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption(),
+        encryption_algorithm=serialization.BestAvailableEncryption(encode_str_utf8(str(PASSWORD))),
     )
 
 
--- utils/pem_to_key.py
+++ utils/pem_to_key.py
@@ -1,10 +1,20 @@
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
+from .encode_to_utf8 import encode_str_utf8
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv('.env')
+
+PASSWORD = os.environ.get("PASSWORD")
 
 
 def read_private_key():
     with open("private_key.pem", "rb") as key_file:
-        private_key = serialization.load_pem_private_key(key_file.read(), password=None, backend=default_backend())
+        private_key = serialization.load_pem_private_key(
+            key_file.read(), password=encode_str_utf8(str(PASSWORD)), backend=default_backend()
+        )
     return private_key
 
 
